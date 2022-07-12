c734120563dd1945e8af37423def38d135031d52
Dennn09 <97225549+Dennn09@users.noreply.github.com>
2022-07-12 13:06:13 +0800

    Responsi2
    Penjualan

---
+++ responsi2/responsi2/Produk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsiuas2377
+{
+    class Produk
+    {
+        public string Nota { get; set; }
+        public string tanggal { get; set; }
+        public string Custumer { get; set; }
+        public string Jenis { get; set; }
+    }
+}
